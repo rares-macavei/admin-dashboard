41f246feba06e02b79c0d686b7d89cb1e018eeae
Rares Macavei <raresmacavei15@gmail.com>
2023-05-19 16:01:13 +0300

    dashboard layout added

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './components/Layout/Layout';
+import Dashboard from './pages/Dashboard/Dashboard';
+// import BoardPage from './pages/Board/Board';
+// import Calendar from './pages/Calendar/Calendar';
+// import DataGrid from './pages/DataGrid/DataGrid';
+
+
+const App = () => {
+  return <div id="dashboard">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+
+          <Route path="dashboard" element={<Dashboard />} />
+          {/* <Route path="calendar" element={<Calendar />} /> */}
+          {/* <Route path="board" element={<BoardPage />} /> */}
+          {/* <Route path="users" element={<DataGrid />} /> */}
+
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  </div>
+};
+
+export default App;
