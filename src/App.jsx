0418582bc125ed0057d129a1621c3676b2a62a16
Rares Macavei <raresmacavei15@gmail.com>
2023-05-19 21:14:30 +0300

    calendar page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from './components/Layout/Layout';
 import Dashboard from './pages/Dashboard/Dashboard';
 // import BoardPage from './pages/Board/Board';
-// import Calendar from './pages/Calendar/Calendar';
+import Calendar from './pages/Calendar/Calendar';
 // import DataGrid from './pages/DataGrid/DataGrid';
 
 
@@ -14,7 +14,7 @@ const App = () => {
         <Route path="/" element={<Layout />}>
 
           <Route path="dashboard" element={<Dashboard />} />
-          {/* <Route path="calendar" element={<Calendar />} /> */}
+          <Route path="calendar" element={<Calendar />} />
           {/* <Route path="board" element={<BoardPage />} /> */}
           {/* <Route path="users" element={<DataGrid />} /> */}
 
